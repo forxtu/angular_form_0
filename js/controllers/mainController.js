34e3d73a04136b419bbfd4250e95335d67ef0d54
forxtu <forxtu@gmail.com>
2017-03-04 00:33:48 +0100

    new submit

---
+++ js/controllers/mainController.js
@@ -0,0 +1,36 @@
+validationApp.controller("mainController", function($scope) {
+    
+    "use strict";
+        
+    $scope.users = [
+        {
+            name : "Denis",
+            username : "forxtu",
+            email : "for@gmail.com",
+            gender : "male"
+        }
+    ];
+    
+    
+    $scope.reset = function() {
+        $scope.user = angular.copy($scope.master);
+    };
+    $scope.reset();
+        
+    
+    $scope.deleteUser = function(user) {
+        $scope.users = $scope.users.filter(function(d) {
+            return d != user;
+        });
+    };
+               
+
+    $scope.submitForm = function() {
+        if ($scope.userForm.$valid) {
+            $scope.users.push($scope.user);
+            $scope.user = "";
+            $scope.userForm.$setUntouched(true);
+        }
+    };
+
+});
